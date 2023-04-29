d6daa895c368092ebbf0bfa0be02a8cc3b4b0bd1
Maria <alece.1414@gmail.com>
2023-04-29 15:42:28 +0300

    feat: add header and specification

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const BODY = document.body;
+
+const HEADER = document.createElement('h1');
+HEADER.classList.add('keyboard__header');
+HEADER.textContent = 'Virtual keyboard';
+BODY.append(HEADER);
+
 const TEXTAREA = document.createElement('textarea');
 
 TEXTAREA.classList.add('keyboard__textarea');
@@ -7,6 +13,11 @@ BODY.append(TEXTAREA);
 const BOARD = document.createElement('div');
 BOARD.classList.add('keyboard__board');
 BODY.append(BOARD);
+
+const SPECIFICATION = document.createElement('p');
+SPECIFICATION.classList.add('keyboard__spec');
+SPECIFICATION.textContent = 'Keyboard is created on Windows';
+BODY.append(SPECIFICATION);
 
 const KEYS_ARRAY = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\\', 'Del', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '/', '▲', 'Shift', 'Ctrl', 'Win', 'Alt', 'Whitespace', 'Alt', '◄', '▼', '►', 'Ctrl'];
 
